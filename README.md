f587e64f2b47b00b3340ad18a2add65f127f55e3
Keterion <miscellaneousmails@protonmail.com>
2025-08-15 17:35:17 +0200

    Start documenting stuff ig

---
+++ README.md
@@ -0,0 +1,16 @@
+# Libtagthing
+
+A collection of functions to share code between tagthing frontends.
+
+## Principles
+
+- Statelessness
+- Keeping it simple
+
+### Statelessness
+Meaning no database so you can move your files however you want because tagthing won't keep track of where they are to associate data with the files.
+
+And because sidecar files are a hassle for now we won't do any of those either (also a lot of formats support XMP or other metadata formats which we'll use)
+
+# Documentation
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,42 @@
 use std::{error::Error, str::FromStr};
 
+/// Trait shared by all structs that will be representing files
 pub trait Media {
+    /// Returns the type of media
     fn media_type(&self) -> MediaType;
+    /// Every struct implementing Media will need to point to a file
     fn path(&self) -> &std::path::Path;
+    /// Check whether the file still exists
     fn exists(&self) -> bool;
+    /// Path for the thumbnail for previews, otherwise None
     fn thumbnail_path(&self) -> Option<&std::path::Path>;
+    /// Date of file creation
     fn date(&self) -> Option<chrono::NaiveDateTime>;
+    /// Filesize in ?
     fn size(&self) -> u64;
 
+    /// Returns true if the file passes specified conditions
     fn matches_filter(filter: &str) -> bool;
+    /// Saves modifications to the struct instance to the associated file
     fn save() -> Result<(), TagError>;
 
+    /// Returns tags, if the file doesn't support tags None is returned
     fn tags(&self) -> Option<&Vec<Tag>>;
+    /// Adds a new tag to the file
     fn add_tag(&self, new_tag: Tag) -> Result<(), TagError>;
+    /// Removes a tag from the file
     fn remove_tag(&self, tag_to_remove: Tag) -> Result<(), TagError>;
+    /// Checks whether the file has the specified tag
     fn has_tag(&self, tag_to_search: Tag) -> bool;
 }
 
+/// Specify type of media for more sophisticated functions and handling
+#[non_exhaustive]
 pub enum MediaType {}
 
 // Tag Struct ========================================================
+
+/// Stores tag name and connections to namespace or other things
 #[derive(PartialEq, Eq, Debug)]
 pub struct Tag {
     tag_string: String,
@@ -42,12 +59,19 @@ impl FromStr for Tag {
 }
 
 // Tag Errors ========================================================
+
+/// Error for tag related operations
 #[derive(Debug)]
 pub enum TagError {
+    /// The file is missing or inaccessible
     FileMissing,
+    /// The file exists but saving was not possible
     OtherSaveError,
+    /// The tag does not exist
     TagMissing,
+    /// The returned character is invalid
     InvalidCharacter(char),
+    /// Other UwU
     UwUpsie,
 }
 
