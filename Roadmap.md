e681619805296f195e668927d98097ffe99226a3
haides002 <105516284+haides002@users.noreply.github.com>
2025-09-01 16:38:58 +0200

    separate thumbnail gen from file update
    we cose to do this because generating thumbnails takes a while. this way
    implementors can chose when and how to do the thumbnail generating

--- Roadmap.md
+++ Roadmap.md
@@ -2,10 +2,11 @@
 
 ## Short Term
 
-- [x] filtering evaluation (https://en.wikipedia.org/wiki/Reverse_Polish_notation#Explanation)
+- [x] filtering evaluation
+      (https://en.wikipedia.org/wiki/Reverse_Polish_notation#Explanation)
 - [ ] basic filetype support
 - [ ] few tests
-- [ ] separate thumbnail generation into own method to support async
+- [x] separate thumbnail generation into own method to support async
 
 ## Mid Term
 
--- src/image.rs
+++ src/image.rs
@@ -28,10 +28,7 @@ impl Image {
 
 impl Media for Image {
     fn update(&mut self) -> Result<(), crate::TagError> {
-        use allmytoes::*;
         use exempi2::{OpenFlags, PropFlags, Xmp, XmpFile, XmpString};
-
-        const THUMBNAIL_SIZE: ThumbSize = ThumbSize::Large;
 
         const EXIF_SCHEMA: &str = "http://ns.adobe.com/exif/1.0/";
         const DUBLIN_CORE_SCHEMA: &str = "http://purl.org/dc/elements/1.1/";
@@ -91,24 +88,8 @@ impl Media for Image {
             }
         }();
 
-        let thumbnail_path = || -> Option<std::path::PathBuf> {
-            let config = AMTConfiguration {
-                force_creation: false,
-                return_smallest_feasible: false,
-                leak: false,
-                custom_provider_spec_file: None,
-                force_inbuilt_provider_spec: false,
-            };
-
-            match AMT::new(&config).get(&self.path, THUMBNAIL_SIZE) {
-                Ok(thumbnail) => Some(std::path::PathBuf::from(&thumbnail.path)),
-                Err(_) => None,
-            }
-        }();
-
         let modified = metadata(&self.path).unwrap().modified().unwrap();
 
-        self.thumbnail_path = thumbnail_path;
         self.date = date;
         self.tags = tags;
         self.modified = modified;
@@ -136,6 +117,29 @@ impl Media for Image {
 
     fn exists(&self) -> bool {
         self.path.exists()
+    }
+
+    fn generate_thumbnail(&mut self) {
+        use allmytoes::*;
+
+        const THUMBNAIL_SIZE: ThumbSize = ThumbSize::Large;
+
+        let thumbnail_path = || -> Option<std::path::PathBuf> {
+            let config = AMTConfiguration {
+                force_creation: false,
+                return_smallest_feasible: false,
+                leak: false,
+                custom_provider_spec_file: None,
+                force_inbuilt_provider_spec: false,
+            };
+
+            match AMT::new(&config).get(&self.path, THUMBNAIL_SIZE) {
+                Ok(thumbnail) => Some(std::path::PathBuf::from(&thumbnail.path)),
+                Err(_) => None,
+            }
+        }();
+
+        self.thumbnail_path = thumbnail_path;
     }
 
     fn thumbnail_path(&self) -> Option<&std::path::Path> {
