8db04890d093c890682f17e88c5803f02c4417ad
haides002 <105516284+haides002@users.noreply.github.com>
2026-03-06 19:17:02 +0100

    add samply to flake

--- flake.nix
+++ flake.nix
@@ -5,30 +5,33 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {nixpkgs, ...}: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    lib = pkgs.lib;
-  in {
-    devShell.x86_64-linux = pkgs.mkShellNoCC {
-      buildInputs = with pkgs; [
-        cargo
-        rustc
-        gcc
+  outputs =
+    { nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      inherit (pkgs) lib;
+    in
+    {
+      devShell.x86_64-linux = pkgs.mkShellNoCC {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          gcc
 
-        pkg-config
+          pkg-config
 
-        bacon
-        rustfmt
-        clippy
-      ];
-
-      nativeBuildInputs = with pkgs; [
-        pkg-config
-      ];
-
-      LD_LIBRARY_PATH =
-        lib.makeLibraryPath [
+          bacon
+          rustfmt
+          clippy
+          samply
         ];
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+
+        LD_LIBRARY_PATH = lib.makeLibraryPath [
+        ];
+      };
     };
-  };
 }
